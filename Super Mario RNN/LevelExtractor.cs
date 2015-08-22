8d04583b6f1e2565d71c32836934cd71b95caf44
terrabite3 <terrabite3@gmail.comm>
2015-08-21 22:30:28 -0700

    Initial commit of empty project

---
+++ Super Mario RNN/LevelExtractor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Super_Mario_RNN
+{
+    class LevelExtractor
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
